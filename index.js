86ac689e2fcf73e22b8ddbdf4f665c5e612b29ce
agn-wita <agn.wita@gmail.com>
2023-01-28 17:10:05 +0100

    main: Added variable to change first paragraph according to game result

--- index.js
+++ index.js
@@ -3,19 +3,20 @@ let secondCard = 11
 let sum = firstCard + secondCard
 let hasBlackjack = false
 let isAlive = false
+let message = ""
 
 if (sum < 21) {
-    console.log("Do you want to draw a new card?")
+    message = "Do you want to draw a new card?"
     isAlive = true
 }
 
 else if (sum === 21) {
-    console.log("You've got Blackjack!") 
+    message = "You've got Blackjack!" 
     hasBlackjack = true 
 }
 
 else {
-    console.log("You're out of the game!") 
+    message = "You're out of the game!" 
 }
 
 
