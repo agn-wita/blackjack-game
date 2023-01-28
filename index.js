8ea7eb92bd4f028c91e463ed467903f0d3e7d600
agn-wita <agn.wita@gmail.com>
2023-01-28 16:58:48 +0100

    main: Added boolean conditions

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 let firstCard = 11
 let secondCard = 11
 let sum = firstCard + secondCard
+let hasBlackjack = false
+let isAlive = false
 
 if (sum < 21) {
     console.log("Do you want to draw a new card?")
+    isAlive = true
 }
 
 else if (sum === 21) {
-    console.log("You've got Blackjack!")  
+    console.log("You've got Blackjack!") 
+    hasBlackjack = true 
 }
 
 else {
