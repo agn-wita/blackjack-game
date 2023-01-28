d9a184c12520fc69509c86c1a96d86fb7cccea82
agn-wita <agn.wita@gmail.com>
2023-01-28 16:56:04 +0100

    main: Added Conditional Statements

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ let firstCard = 11
 let secondCard = 11
 let sum = firstCard + secondCard
 
+if (sum < 21) {
+    console.log("Do you want to draw a new card?")
+}
+
+else if (sum === 21) {
+    console.log("You've got Blackjack!")  
+}
+
+else {
+    console.log("You're out of the game!") 
+}
 
 
 
