42b98acbbeffa99cb2d199cc6b5e1912b00a96d2
agn-wita <agn.wita@gmail.com>
2023-01-28 17:24:20 +0100

    main: Added function starting the game

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-let firstCard = 1
+let firstCard = 12
 let secondCard = 11
 let sum = firstCard + secondCard
 let hasBlackjack = false
 let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
 
-if (sum < 21) {
-    message = "Do you want to draw a new card?"
-    isAlive = true
+function startGame() {
+    sumEl.textContent = "Sum: " + sum
+    if (sum < 21) {
+        message = "Do you want to draw a new card?"
+        isAlive = true
+    }
+    
+    else if (sum === 21) {
+        message = "You've got Blackjack!" 
+        hasBlackjack = true 
+    }
+    
+    else {
+        message = "You're out of the game!" 
+    }
+    
+    messageEl.textContent = message
 }
 
-else if (sum === 21) {
-    message = "You've got Blackjack!" 
-    hasBlackjack = true 
-}
-
-else {
-    message = "You're out of the game!" 
-}
-
-messageEl.textContent = message
 
 
 
