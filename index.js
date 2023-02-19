608cfaca39b1abdf5c51a79a0982db965a2c889f
agn-wita <agn.wita@gmail.com>
2023-02-19 15:42:56 +0100

    main: Added info about players chips

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let player = {
+    name: "John",
+    chips: 200
+}
 let cards = []
 let sum = 0
 let hasBlackjack = false
@@ -6,6 +10,8 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardEl = document.getElementById("card-el")
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + " $" + player.chips
 
 function getRandomCard() {
     // if 1     -> return 11
@@ -69,10 +75,3 @@ function newCard() {
         renderGame()
     }
 }
-
-
-
-
-
-
-
